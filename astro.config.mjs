f2c5f24cda811aba1787b12e88e2081213f83f78
sicilo <mejiamonsalve.camilo@gmail.com>
2024-01-29 23:45:25 -0500

    updating base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site:'https://sicilo.pages.github.io',
-  base:'/landing-tech',
+  base:'/tech-landing',
   integrations: [tailwind(), react({
     include: ['**/React/*']
   })]
